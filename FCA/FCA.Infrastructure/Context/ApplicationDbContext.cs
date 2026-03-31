7604dee2b398e490b3cb5824f57c963b099c9078
Fábio Couto Araujo <fabiocoutoaraujo@gmail.com>
2026-03-31 08:53:25 -0300

    adicionando e configurando a classe ApplicationDbContext no projeto Repository

---
+++ FCA/FCA.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FCA.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FCA.Infrastructure.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        : base (options)
+    { }
+
+    public DbSet<Proprietario>? Proprietarios { get; set; }
+    public DbSet<Veiculo>? Veiculos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
