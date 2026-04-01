343331a879be1d2489e4e684f79c9975fa111bc1
Fábio Couto Araujo <fabiocoutoaraujo@gmail.com>
2026-04-01 13:39:23 -0300

    habilitando e exibindo comentário xml na documentação do swagger

--- FCA/FCA.API/Program.cs
+++ FCA/FCA.API/Program.cs
@@ -1,6 +1,7 @@
 using FCA.API.Filters;
 using FCA.CrossCutting.IoC;
 using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +17,11 @@ builder.Services.AddSwaggerGen(opt =>
                        Title = "Evertec Teste Backend de FCA.",
                        Description = "API para um CRUD completo de Proprietários e Veículos."
                    });
+
+    // Configurando o Swagger para utilizar os comentários XML das controllers
+    var xmlFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlFullPath = Path.Combine(AppContext.BaseDirectory, xmlFileName);
+    opt.IncludeXmlComments(xmlFullPath);
 });
 
 var app = builder.Build();
